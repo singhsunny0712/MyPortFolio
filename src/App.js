60e95b17cacb9fa71f747bc93ad571b02755b8b0
Sunny Singh <singhsunny0712@gmail.com>
2020-11-01 15:00:27 +0530

    carousal added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 //compoents
 import './App.css';
-import MyNavbar from "../src/components/my-navbar/mynavbar.components"
+import MyNavbar from "../src/components/my-navbar/mynavbar.components";
+import MyCarousal from "../src/components/my-carousal/my-carousal.components";
+import TypeWriter from "../src/components/title-message/title-message.component";
 
 const App = () => {
   return (
     <div>
-    <MyNavbar />
-    My Portfolio</div>
+      <MyNavbar />
+      <MyCarousal />
+      <TypeWriter />
+    </div>
   );
 }
 
