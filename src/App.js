4ab80a4bd3fb0561b1586c456f11c65a5e1d7a88
Sunny Singh <singhsunny0712@gmail.com>
2020-11-03 12:30:26 +0530

    Added skill section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Fade from 'react-reveal/Fade'
-import { Parallax } from "react-parallax";
+import Fade from 'react-reveal/Fade';
+import Slide from "react-reveal/Slide";
+// import { Parallax } from "react-parallax";
 import Container from "react-bootstrap/Container";
 
 //compoents
@@ -9,6 +10,7 @@ import MyNavbar from "../src/components/my-navbar/mynavbar.components";
 import MyCarousal from "../src/components/my-carousal/my-carousal.components";
 import TypeWriter from "../src/components/title-message/title-message.component";
 import About from "./pages/about/about.component";
+import Skills from "./pages/skill/skills.component";
 
 
 const App = () => {
@@ -26,32 +28,24 @@ const App = () => {
           // strength={-200}
       > */}
       <Container className="container-box rounded">
-      <Fade duration={500}>
+      <Fade duration={700}>
         <About />
 
       </Fade>
 
       </Container>
-        
-
       {/* </Parallax> */}
       </div>
-      {/* <div>
-        <Parallax
-          blur={{ min: -30, max: 30 }}
-          bgImage={require("./assets/img/parallex/background.webp")}
-          bgImageAlt="paralaax img"
-          strength={-200}
-        >
-          <div>
-            <Container className="container-box rounded">
-              <Fade duration={500}>
-                <About />
-              </Fade>
-            </Container>
-          </div>
-        </Parallax>
-      </div> */}
+
+      
+      {/* Skill Section */}
+      <Container className="container-box rounded">
+        <Slide bottom duration={700}>
+          <hr />
+          <Skills />
+        </Slide>
+      </Container>
+      
     </div>
   );
 }
